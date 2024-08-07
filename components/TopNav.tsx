dfda0de7b746d117d6c1fec8946e5211e0d900d9
The Wizard of Hahz <info@luvnft.com>
2024-08-07 16:23:51 -0400

    updated

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -7,10 +7,11 @@ export async function TopNav() {
   const { object: globalSettings } = await cosmic.objects
     .findOne({
       type: "settings",
-      slut: "settings",
+      slug: "settings",
     })
     .props("metadata")
     .depth(1);
+    
   return (
     <div className="w-full gap-4 bg-white dark:bg-black z-10 h-[70px] top-0 fixed border-b dark:border-gray-800 items-center flex px-4">
       <Link href="/" className="items-center flex dark:text-gray-100">
