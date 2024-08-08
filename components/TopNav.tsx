31c53507b8749171110f83be0a241bc4261f4918
The Wizard of Hahz <info@luvnft.com>
2024-08-07 20:40:02 -0400

    updated logo

--- components/TopNav.tsx
+++ components/TopNav.tsx
@@ -15,7 +15,7 @@ export async function TopNav() {
     <div className="w-full gap-4 bg-white dark:bg-black z-10 h-[70px] top-0 fixed border-b dark:border-gray-800 items-center flex px-4">
       <Link href="/" className="items-center flex dark:text-gray-100">
         <img
-          src="https://i.imgur.com/oaW3h8y.png" // Path to your PNG logo
+          src="https://i.imgur.com/9xpFqvy.png" // Path to your PNG logo
           alt="Site Logo"
           className="h-8 w-auto mr-2" // Adjust size as needed
         />
